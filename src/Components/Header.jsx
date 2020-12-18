387d36cfaeed29b73ae21ff5999e4887251238f1
Sean Oyler <sean.oyler@gmail.com>
2020-12-17 19:54:57 -0800

    adds some styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <div>
+      <section className="header">
+        <h1 className="name">Sean Oyler</h1>
+        <aside className="email">
+          <button>Download PDF</button>
+          <p>
+            <a href="mailto:sean.oyler@gmail.com">sean.oyler@gmail.com</a>
+          </p>
+          <p>(250)-510-3457</p>
+        </aside>
+      </section>
+      <section id = "tag-line">
+        <h2 className="tag-line">Junior Web Developer</h2>
+        <p className="social">Test</p>
+      </section>
+    </div>
+  );
 };
 
 export default Header;
