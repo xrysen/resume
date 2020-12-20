8f421188645ff7acd1f9c6302bbf3847fd713603
Sean Oyler <sean.oyler@gmail.com>
2020-12-19 20:09:19 -0800

    Adds props and displays education for Projects and ProjectListItems components

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { education } from "../data/data";
+import EducationListItem from "./EducationListItem";
 
 const Education = () => {
+  const schools = education.map((item) => {
+    return (
+      <EducationListItem
+        key={item.id}
+        school={item.school}
+        dates={item.dates}
+        diploma={item.diploma}
+      />
+    );
+  });
+
   return (
-    <h1>Education</h1>
+    <section>
+      <h1>Education</h1>
+      <ul>{schools}</ul>
+    </section>
   );
 };
 
--- src/Components/EducationListItem.jsx
+++ src/Components/EducationListItem.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 
 const EducationListItem = (props) => {
-
   return (
     <li>
-      <h3>{props.school}</h3>
-      <p><em>{props.diploma}</em></p>
-      <p>{props.dates}</p>
+      <h3>
+        {props.school}
+        <span className="dates">{props.dates}</span>
+      </h3>
+      <p>
+        <em>{props.diploma}</em>
+      </p>
     </li>
-  )
-}
+  );
+};
 
 export default EducationListItem;
