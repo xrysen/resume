db646fdac366691b287ca50aef462b2ff79881e2
Sean Oyler <sean.oyler@gmail.com>
2020-12-20 14:45:23 -0800

    Adds styling

--- src/Components/Jobs.jsx
+++ src/Components/Jobs.jsx
@@ -3,22 +3,24 @@ import JobTaskItem from "./JobTaskItem";
 
 const Jobs = (props) => {
   const list = props.description.map((listItem) => {
-    return <JobTaskItem description = {listItem} />
-  })
- 
+    return <JobTaskItem description={listItem} />;
+  });
+
   return (
     <li key={props.id}>
-      <h3>{props.company}</h3>
-      <strong>{props.location}</strong>
-      <br />
-      {props.dates}
+      <header className="jobDates">
+        <h3>{props.company}</h3>
+        <div>
+          <strong>{props.location}</strong>
+          <br />
+          <strong>{props.dates}</strong>
+        </div>
+      </header>
 
       <p>
         <em>{props.title}</em>
       </p>
-      <ul className = "jobTasks">
-        {list}
-      </ul>
+      <ul className="jobTasks">{list}</ul>
     </li>
   );
 };
