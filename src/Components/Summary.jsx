b9284c3af606f1a6e8afc2ebd09bbb5e4abf977e
Sean Oyler <xrysen@gmail.com>
2021-03-25 01:07:14 -0700

    Fixes typo

--- src/Components/Summary.jsx
+++ src/Components/Summary.jsx
@@ -5,7 +5,7 @@ const Summary = () => {
     <section className="summary">
       <h1>Summary</h1>
       <p>
-        I am persuing my life long passion of becoming a software developer, transitioning from retail management. Having spent
+        I am pursuing my life long passion of becoming a software developer, transitioning from retail management. Having spent
         a lot of my youth working on game development projects, and other
         programming related tasks, I have always loved solving problems through
         code and seeing those solutions appear on screen.
